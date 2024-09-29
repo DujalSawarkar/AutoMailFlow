a9a1fedba8b6494f68c2f3e163888b0bd01fce99
DujalSawarkar <dujalsawar5@gmail.com>
2024-09-29 09:07:48 +0530

    feat : attached resume to email template

--- src/emailService.ts
+++ src/emailService.ts
@@ -49,8 +49,15 @@ const sendEmail = async (email: string) => {
         qualifications and experiences. I would appreciate the opportunity to discuss 
         how my skills and aspirations align with any full-time opportunities available.
       </p>
-      <p>Thank you for considering my application.</p>
+      <p>Thank you.</p>
     `,
+    attachments: [
+      {
+        filename: 'Dujal_Sawarkar_Resume.pdf', // Change this to the actual filename
+        path: './DujalSawarkar_Resume.pdf', // Update the path to your resume file
+        contentType: 'application/pdf',
+      },
+    ],
   };
 
   try {
@@ -62,6 +69,40 @@ const sendEmail = async (email: string) => {
 };
 
 
+// const mailOptions = {
+//   from: `"Dujal Sawarkar" <${process.env.SMTP_USER}>`,
+//   to: "dujalsawar5@gmail.com",
+//   subject: "Job Application or Referral",
+//   html: `
+//     <p>Dear HR,</p>
+//     <p>
+//       My name is <strong>Dujal Sawarkar</strong>, and I am currently pursuing a 
+//       <strong>Computer Engineering degree</strong>. I have a strong foundation in 
+//       programming languages such as <strong>C++</strong>, <strong>JavaScript</strong>, and <strong>TypeScript</strong>. 
+//       My expertise includes web technologies like <strong>HTML</strong>, 
+//       <strong>CSS</strong>, <strong>React</strong>, <strong>Next.js</strong>, 
+//       <strong>Vue.js</strong>, <strong>Node.js</strong>, <strong>Express.js</strong>, 
+//       <strong>SQL</strong>, and <strong>MongoDB</strong>.
+//     </p>
+//     <p>
+//       I am eager to leverage my technical skills and passion for learning to 
+//       contribute to your team's success while gaining valuable industry experience.
+//     </p>
+//     <p>
+//       Enclosed is my resume for your review, which provides further details on my 
+//       qualifications and experiences. I would appreciate the opportunity to discuss 
+//       how my skills and aspirations align with any full-time opportunities available.
+//     </p>
+//     <p>Thank you for considering my application.</p>
+//   `,
+//   attachments: [
+//     {
+//       filename: 'Dujal_Sawarkar_Resume.pdf', // Change this to the actual filename
+//       path: './DujalSawarkar_Resume.pdf', // Update the path to your resume file
+//       contentType: 'application/pdf',
+//     },
+//   ],
+// };
 
 // for 1 mail
 // const sendonemail = () => {
